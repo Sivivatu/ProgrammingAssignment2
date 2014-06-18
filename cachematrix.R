c986dcda8472a2956f88f2009b3401fa46f0de80
Siviatu <paul.j.houghton@gmail.com>
2014-06-18 21:59:56 +1200

    First submission
    Transposed the example code, changed function to Solve(), added
    comments.

--- cachematrix.R
+++ cachematrix.R
@@ -2,24 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
+# The below function completes the following tasks:
+# 1 sets a matrix to a vector
+# 2 gets the matrix from the vector
+# 3 sets the inverse of the matrix
+# 4 gets the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                x <<- y    #superassigns m to the global environment
+                m <<- NULL #superassigns m to the global environment
         }
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+#the below function checks to see if inverse is cached in the global environment
+#with the m variable. if it is (m =/= NULL) it returns m
+#otherwise it calculates the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        message("calculating inverse")
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
